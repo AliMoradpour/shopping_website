2d7eeb8219dbccf20b52e991dcd7164f4c8d81ef
AliMoradpour <mr.mrdp@yahoo.com>
2023-04-22 00:50:47 +0430

    write contexes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import Cart from "./Pages/Cart";
 import "./App.css";
+import CartProvider from "./Providers/CartProvider";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/cart" component={Cart} />
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <CartProvider>
+        <Switch>
+          <Route path="/cart" component={Cart} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </CartProvider>
     </Router>
   );
 }
