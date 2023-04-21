5c73b15c7177930b0b050e665ba5ceafe083c82d
AliMoradpour <mr.mrdp@yahoo.com>
2023-04-22 01:37:07 +0430

    add styles to cart list

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,15 +1,43 @@
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
+import "./cartPage.css";
 
 const Cart = () => {
-  const {cart} = useCart();
+  const { cart } = useCart();
+
+  if (!cart.length) {
+    return (
+      <Layout>
+        <main>
+          <h2>Cart is Empty !</h2>
+        </main>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <main>{cart.length ? cart.map(item => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-        </div>
-      )) : <p>No Item in Cart</p>}</main>
+      <main className="container">
+        <section className="cartCenter">
+          <section className="cartItemList">
+            {cart.map((item) => (
+              <div key={item.id} className="cartItem">
+                <div className="itemImage">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div>{item.name}</div>
+                <div>$ {item.price * item.quantity}</div>
+                <div className="cartBtns">
+                  <button className="btn primary">Add</button>
+                  <div>{item.quantity}</div>
+                  <button className="btn">Remove</button>
+                </div>
+              </div>
+            ))}
+          </section>
+          <section className="cartSummery">cart summery</section>
+        </section>
+      </main>
     </Layout>
   );
 };
